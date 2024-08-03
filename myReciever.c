86b635eaf6a0095cfb6e7c6a075c22da262f07ef
Rainbowheat <104723100+Rainbowheat@users.noreply.github.com>
2024-08-03 17:37:13 +1000

    Made changes

---
+++ myReciever.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MYPORT "4950"
+
+#define MAXBUFLEN 100
+
+void *get_in_addr(struct sockaddr *sa)
+{
+  if(sa->sa_family == AF_INET) 
+  {
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(void)
+{
+  int sockfd;
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+  int numbytes;
+  struct sockaddr_storage their_address;
+  char buf[MAXBUFLEN];
+  socklen_t addr_len;
+  char s[INET6_ADDRSTRLEN];
+
+  memset(&hints, 0, sizeof(hints));
+
+  hints.ai_family = AF_INET6;
+  hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  
+
+  
+}
--- tcp.c
+++ tcp.c
@@ -1,5 +1,4 @@
 #include <arpa/inet.h>
-#include <assert.h>
 #include <errno.h>
 #include <netdb.h>
 #include <netinet/in.h>
@@ -10,29 +9,30 @@
 #include <time.h>
 #include <unistd.h>
 
-#define ERROR(cond, msg) assert((printf((cond) ? msg : '\0'), cond))
-
-void errHnd(int value, char *message, int errorVal) {
+int errHnd(int value, char *message, int errorVal) {
   if (value == errorVal) {
-    fprintf(stderr, "%s\n", message);
+    printf("%s\n", message);
+    return 1;
   }
+  return 0;
 }
 
 int main(void) {
-  long error;
   printf("configuring local address\n");
 
   struct addrinfo hints;
 
-  memset(&hints, 0, sizeof(hints) / sizeof(char));
+  memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
 
   struct addrinfo *bind_address;
 
-  error = getaddrinfo(0, "8080", &hints, &bind_address);
-  ERROR(error == -1, "getaddrinfo failed");
+  if (getaddrinfo(0, "8080", &hints, &bind_address) == -1) {
+    printf("getaddrinfo failed");
+    return 1;
+  };
 
   printf("CREATING SOCKET");
   int socket_listen;
@@ -56,45 +56,45 @@ int main(void) {
 
   printf("Waiting for connections");
 
-  struct sockaddr_storage client_addresses;
+    struct sockaddr_storage client_addresses;
 
-  socklen_t client_len = sizeof(client_addresses);
+    socklen_t client_len = sizeof(client_addresses);
 
-  int socket_client =
-      accept(socket_listen, (struct sockaddr *)&client_addresses, &client_len);
+    int socket_client = accept(
+        socket_listen, (struct sockaddr *)&client_addresses, &client_len);
 
-  errHnd(socket_client, "Sock Client Failed", -1);
+    errHnd(socket_client, "Sock Client Failed", -1);
 
-  printf("client connected");
+    printf("client connected");
 
-  char address_buffer[100];
-  getnameinfo((struct sockaddr *)&client_addresses, client_len, address_buffer,
-              sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
+    char address_buffer[100];
+    getnameinfo((struct sockaddr *)&client_addresses, client_len,
+                address_buffer, sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
 
-  printf("%s\n", address_buffer);
+    printf("%s\n", address_buffer);
 
-  printf("reading a request");
+    printf("reading a request");
 
-  char request[1024];
+    char request[1024];
 
-  int bytesRecieved = recv(socket_client, request, 1024, 0);
+    int bytesRecieved = recv(socket_client, request, 1024, 0);
 
-  printf("%.*s\n", bytesRecieved, request);
+    printf("%.*s\n", bytesRecieved, request);
 
-  time_t timer;
-  time(&timer);
-  char *time_message = ctime(&timer);
+    time_t timer;
+    time(&timer);
+    char *time_message = ctime(&timer);
 
-  const char *response = "HTTP/1.1 200 OK\r\n"
-                         "Connection: close\r\n"
-                         "Content-Type: text/plain\r\n\r\n"
-                         "Local time is: ";
+    const char *response = "HTTP/1.1 200 OK\r\n"
+                           "Connection: close\r\n"
+                           "Content-Type: text/plain\r\n\r\n"
+                           "Local time is: ";
 
-  int byte_sent = send(socket_client, response, strlen(response), 0);
-  byte_sent = send(socket_client, time_message, strlen(time_message), 0);
+    int byte_sent = send(socket_client, response, strlen(response), 0);
+    byte_sent = send(socket_client, time_message, strlen(time_message), 0);
 
-  close(socket_client);
-  close(socket_listen);
-
+    close(socket_client);
+    close(socket_listen);
+  
   return 0;
 }
