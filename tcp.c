be8a16db4a860204b4469b3a25fca4c7ab3cbbea
Katalysmus <katalysmus@gmail.com>
2024-08-03 07:53:57 +0100

    Added an error handling macro
    Google comma operator: It evaluates the left hand of the comma
    expression then discards its return, then it returns the right hand
    value of the expression
    If condition is TRUE it will output the message and crash, else it will
    print nothing

--- tcp.c
+++ tcp.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <assert.h>
 #include <errno.h>
 #include <netdb.h>
 #include <netinet/in.h>
@@ -9,30 +10,29 @@
 #include <time.h>
 #include <unistd.h>
 
-int errHnd(int value, char *message, int errorVal) {
+#define ERROR(cond, msg) assert((printf((cond) ? msg : '\0'), cond))
+
+void errHnd(int value, char *message, int errorVal) {
   if (value == errorVal) {
-    printf("%s\n", message);
-    return 1;
+    fprintf(stderr, "%s\n", message);
   }
-  return 0;
 }
 
 int main(void) {
+  long error;
   printf("configuring local address\n");
 
   struct addrinfo hints;
 
-  memset(&hints, 0, sizeof(hints));
+  memset(&hints, 0, sizeof(hints) / sizeof(char));
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
 
   struct addrinfo *bind_address;
 
-  if (getaddrinfo(0, "8080", &hints, &bind_address) == -1) {
-    printf("getaddrinfo failed");
-    return 1;
-  };
+  error = getaddrinfo(0, "8080", &hints, &bind_address);
+  ERROR(error == -1, "getaddrinfo failed");
 
   printf("CREATING SOCKET");
   int socket_listen;
@@ -56,45 +56,45 @@ int main(void) {
 
   printf("Waiting for connections");
 
-    struct sockaddr_storage client_addresses;
+  struct sockaddr_storage client_addresses;
 
-    socklen_t client_len = sizeof(client_addresses);
+  socklen_t client_len = sizeof(client_addresses);
 
-    int socket_client = accept(
-        socket_listen, (struct sockaddr *)&client_addresses, &client_len);
+  int socket_client =
+      accept(socket_listen, (struct sockaddr *)&client_addresses, &client_len);
 
-    errHnd(socket_client, "Sock Client Failed", -1);
+  errHnd(socket_client, "Sock Client Failed", -1);
 
-    printf("client connected");
+  printf("client connected");
 
-    char address_buffer[100];
-    getnameinfo((struct sockaddr *)&client_addresses, client_len,
-                address_buffer, sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
+  char address_buffer[100];
+  getnameinfo((struct sockaddr *)&client_addresses, client_len, address_buffer,
+              sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
 
-    printf("%s\n", address_buffer);
+  printf("%s\n", address_buffer);
 
-    printf("reading a request");
+  printf("reading a request");
 
-    char request[1024];
+  char request[1024];
 
-    int bytesRecieved = recv(socket_client, request, 1024, 0);
+  int bytesRecieved = recv(socket_client, request, 1024, 0);
 
-    printf("%.*s\n", bytesRecieved, request);
+  printf("%.*s\n", bytesRecieved, request);
 
-    time_t timer;
-    time(&timer);
-    char *time_message = ctime(&timer);
+  time_t timer;
+  time(&timer);
+  char *time_message = ctime(&timer);
 
-    const char *response = "HTTP/1.1 200 OK\r\n"
-                           "Connection: close\r\n"
-                           "Content-Type: text/plain\r\n\r\n"
-                           "Local time is: ";
+  const char *response = "HTTP/1.1 200 OK\r\n"
+                         "Connection: close\r\n"
+                         "Content-Type: text/plain\r\n\r\n"
+                         "Local time is: ";
 
-    int byte_sent = send(socket_client, response, strlen(response), 0);
-    byte_sent = send(socket_client, time_message, strlen(time_message), 0);
+  int byte_sent = send(socket_client, response, strlen(response), 0);
+  byte_sent = send(socket_client, time_message, strlen(time_message), 0);
 
-    close(socket_client);
-    close(socket_listen);
-  
+  close(socket_client);
+  close(socket_listen);
+
   return 0;
 }
